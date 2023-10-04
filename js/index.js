19a6a843b2cc5a7caf54aaac8932e519d8804a2e
CarolinaSanches24 <carolmosilva34@gmail.com>
2023-10-04 00:51:20 -0300

    Menu responsivo ok

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const navbar = document.querySelector('.navbar');
 const menuButton = document.querySelector('.menu-button');
 
-menuButton.addEventListener('click', function () {
+menuButton.addEventListener('click', () => {
     navbar.classList.toggle('show-menu');
 });
